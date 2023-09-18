9152e9efe1b5ccc5b18b76077cc88f534c82967c
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-18 15:05:28 +0200

    [add] cicle for square generation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 // 5. Definisco la variabile del container
 const container = document.querySelector('.container');
 
-// 6.
 
-const newSquare = document.createElement('div');
-newSquare.className = 'square';
 
-console.log(newSquare);
 
 reset();
 
+let square;
+
+// 8. Creo il ciclo di generazione dei 100 quadrati
+for (let i = 1; i <= 100; i++){
+  square = [i];
+
+  generateSquare(i);
+
+
+};
+
+
+// 6. Definisco la funzione di generazione del quadrato, dando un custom ID basato sull'index del ciclo 
+
+function generateSquare(index){
+  let newSquare = document.createElement('div');
+  newSquare.className = 'square';
+  newSquare._squareID = index;
+  container.append(newSquare);
+}
 
 
 
-
+// 7. Creo la funzione di reset del container e la invoco
 function reset(){
   container.innerHTML = '';
 };
