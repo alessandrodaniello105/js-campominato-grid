8c6a8e7551a1c0cf33ab7f8f555e168c164045a2
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-18 14:55:11 +0200

    [add]: reset function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+// 5. Definisco la variabile del container
+const container = document.querySelector('.container');
+
+// 6.
+
+const newSquare = document.createElement('div');
+newSquare.className = 'square';
+
+console.log(newSquare);
+
+reset();
+
+
+
+
+
+function reset(){
+  container.innerHTML = '';
+};
